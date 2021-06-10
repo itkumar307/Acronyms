f0993953b66a89a88fc87ad6f85aabec70abae44
Kumaravel <464082@cognizant.com>
2021-06-10 11:04:42 +0530

    organize import

--- app/src/main/java/com/kumar/acronyms/data/api/ApiService.kt
+++ app/src/main/java/com/kumar/acronyms/data/api/ApiService.kt
@@ -5,6 +5,9 @@ import com.kumar.acronyms.data.model.AcronymsResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+/**
+ * Retrofit API service
+ */
 interface ApiService {
     @GET("software/acromine/dictionary.py")
     suspend fun getAcromine(@Query("sf") sf: String): List<AcronymsResponse>
--- app/src/main/java/com/kumar/acronyms/ui/adapter/AcronymsAdapter.kt
+++ app/src/main/java/com/kumar/acronyms/ui/adapter/AcronymsAdapter.kt
@@ -1,13 +1,10 @@
 package com.kumar.acronyms.ui.adapter
 
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.kumar.acronyms.R
-import com.kumar.acronyms.utils.Logger
 import kotlinx.android.synthetic.main.item_layout.view.*
 
 
